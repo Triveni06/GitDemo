cf92a349269afd402a4a9f7903a85ebfe0f7df01
Triveni06 <triveniusa2@gmail.com>
2021-03-02 09:54:45 -0500

    Second commit2

---
+++ src/testcases/Searchphone.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Searchphone {
+	@Test(priority=1)
+	public void search() {
+		System.out.println("searching phone");
+	}
+	@Test(priority=2)
+	public void selectphone() {
+		System.out.println("select phone");
+	}
+	@Test (priority=3)
+	public void checkout() {
+		System.out.println("check out phone");
+	}
+
+}
